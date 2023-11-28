321ec8cbf63c65c5c33391201525e5d02f37e3d5
jeprayogo <jannyekaprayogo@gmail.com>
2023-11-28 10:56:54 +0700

    updated js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,3 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 $(document).ready(function() {
     // Hide the preloader when the page is fully loaded
